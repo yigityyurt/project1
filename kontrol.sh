2f3d95b107a091ed539470ab9672cf9864e14faf
Yigit Yurt <yigit.yurt@corp.setcard.com.tr>
2026-08-31 15:07:31 +0300

    kontrol.sh dosyasi saf LF formatinda yeniden olusturuldu

--- kontrol.sh
+++ kontrol.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-echo "Test 1: Matematiksel sonuc bekleniyor.."
+echo "Test 1: Matematiksel kontrol basliyor..."
 if [ 2 -eq 2 ]; then
     echo "Test 1: Basarili.."
-else 
+else
     echo "Test 1: Basarisiz.."
     exit 1
 fi
 echo "Test 2: README var mi kontrol ediliyor"
-if [ -f README.md]; then
-    echo:"Test 2: README var.."
-else  
-    echo:"Test 2: README yok.."
+if [ -f "README.md" ]; then
+    echo "Test 2: Basarili, README mevcut."
+else
+    echo "Test 2: README yok.."
     exit 1
 fi
